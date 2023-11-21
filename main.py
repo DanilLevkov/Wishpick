875ff18df9ddf62179787a09d37d4c7a283b947f
--get <levkow.da@gmail.com>
2023-11-21 14:51:03 +0300

    refactor config to use .env file instead of .ini

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
+import asyncio
 import logging
 
-from decouple import config
-
-import pandas as pd
 from aiogram import Bot, Dispatcher
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import StatesGroup, State
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.utils import executor
-from aiogram.types import Message
+from aiogram.fsm.storage.memory import MemoryStorage
 
-logging.basicConfig(level=logging.INFO)
+from utils.config_reader import config
 
-bot = Bot(token=config('SECRET_TOKEN'))
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage = storage)
 
-@dp.message_handler(commands=['start'])
-async def start_command(message: Message):
-    await message.answer("Привет")
-    await message.answer_sticker(r"CAACAgIAAxkBAAEIt5xkR4HL9QTpWYk6iQ6AgbxEPnDLqQACDioAAt1nQUpP0mg9dWgrSi8E")
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    )
+
+    dp = Dispatcher(storage=MemoryStorage())
+    bot = Bot(config.bot_token.get_secret_value())
+
+    # dp.include_router(common.router)
+
+    await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    asyncio.run(main())
---
+++ utils/config_reader.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+
+class Settings(BaseSettings):
+    # SecretStr for more security
+    bot_token: SecretStr
+
+    # Make sure that .env has UTF-8 encoding
+    model_config = SettingsConfigDict(env_file='../.env', env_file_encoding='utf-8')
+
+
+# Load
+config = Settings()
