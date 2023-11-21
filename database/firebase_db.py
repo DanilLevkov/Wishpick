0af9ea724ccafa0697fb0b87ad2d2028e5b8f9ea
--get <levkow.da@gmail.com>
2023-11-21 23:33:42 +0300

    add async db methods

--- database/firebase_db.py
+++ database/firebase_db.py
@@ -2,8 +2,7 @@ import asyncio
 import concurrent.futures
 
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin.db import Reference
+from firebase_admin import credentials, db
 
 from credentials.config_reader import config
 
@@ -17,6 +16,18 @@ firebase_admin.initialize_app(cred, {
 db_executor = concurrent.futures.ThreadPoolExecutor(max_workers=config.db_max_workers)
 
 
-async def get_response(ref: Reference):
+async def db_get(ref: db.Reference):
     return await asyncio.get_running_loop().run_in_executor(db_executor, ref.get)
+
+
+async def db_set(ref: db.Reference, value):
+    return await asyncio.get_running_loop().run_in_executor(db_executor, ref.set, value)
+
+
+async def db_push(ref: db.Reference) -> db.Reference:
+    return await asyncio.get_running_loop().run_in_executor(db_executor, ref.push)
+
+
+async def db_update(ref: db.Reference, value):
+    return await asyncio.get_running_loop().run_in_executor(db_executor, ref.push, value)
 
