4aa33f233e6c5dc8d9acf45620d5330c52cf5566
--get <levkow.da@gmail.com>
2023-11-27 13:57:21 +0300

    impl web_parser_connector.py

---
+++ bot/utils/web_parser_connector.py
@@ -0,0 +1,30 @@
+import aiohttp
+import asyncio
+import urllib.parse
+
+from credentials.config_reader import config
+
+
+class URLParser:
+    def __init__(self, host: str, port: str):
+        self._session = None
+        self._api_url = "http://" + host + ":" + port
+        self._callback_url = self._api_url + "/callback?"
+
+    def start_session(self):
+        self._session = aiohttp.ClientSession(loop=asyncio.get_running_loop())
+
+    async def check_connection(self):
+
+        async with self._session.get(self._api_url) as resp:
+            return resp.text()
+
+    async def wait_response(self, url: str):
+        request = self._callback_url + urllib.parse.urlencode({"url": url})
+        async with self._session.get(request) as resp:
+            if resp.status != 200:
+                return
+            return await resp.json()
+
+
+url_parser = URLParser(str(config.web_parser_host), str(config.web_parser_port))
